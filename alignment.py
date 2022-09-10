d6d32a33b799827fbbea9f4fbf5842e031458ba7
viniwell <viniwell2807@gmail.com>
2022-09-10 15:37:58 +0100

    add alingment.py

---
+++ alignment.py
@@ -0,0 +1,16 @@
+number=20
+print(f'{number}')
+
+print(f'{number:8}')
+
+print(f'{number:>8}')
+
+print(f'{number:<8}')
+
+print(f'{number:_<8}')
+
+
+x=20.123
+print(f'{x:0<8}')
+
+print(f'{x:.5f}')
